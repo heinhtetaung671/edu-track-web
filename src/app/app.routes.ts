da10fb0c1f63bbd9feb1cad77a7fcc80629f965d
Hein Htet Aung <hhein8358@gmail.com>
2024-06-25 00:47:36 +0630

    add logout button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseManagementComponent } from './pages/managements/course-management/course-management.component';
 import { ManagementsComponent } from './pages/managements/managements.component';
+import { CategoryManagementComponent } from './pages/managements/category-management/category-management.component';
 
 const mainTitle = 'Edu-Track';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: `Home | ${mainTitle}`, pathMatch: 'full'},
   { path: 'management', component: ManagementsComponent, children: [
-    { path: 'course', component: CourseManagementComponent, title: `Course-Managemet | ${mainTitle}`}
+    { path: 'course', component: CourseManagementComponent, title: `Course-Managemet | ${mainTitle}`},
+    { path: 'category', component: CategoryManagementComponent, title: `Category-Management | ${mainTitle}`}
   ]}
 ];
---
+++ src/app/pages/managements/category-management/category-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-management.component.html',
+  styles: ``
+})
+export class CategoryManagementComponent {
+
+}
