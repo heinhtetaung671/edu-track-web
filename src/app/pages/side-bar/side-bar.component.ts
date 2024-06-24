41070698a6a4e34ff82e603422f9929c0ba87aaa
Hein Htet Aung <hhein8358@gmail.com>
2024-06-24 20:02:29 +0630

    side bar theme finish

--- src/app/pages/side-bar/side-bar.component.ts
+++ src/app/pages/side-bar/side-bar.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { WidgetsModule } from '../../widgets/widgets.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [WidgetsModule],
+  imports: [WidgetsModule, CommonModule],
   templateUrl: './side-bar.component.html',
   styles: ``
 })
 export class SideBarComponent {
+
+  iconSize = signal<any>('size-7');
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#003366',
+      }
+    },
   },
   plugins: [],
 }
