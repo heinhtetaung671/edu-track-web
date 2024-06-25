205cc9408f635fc206048c966424ce7c0cae1e94
Hein Htet Aung <hhein8358@gmail.com>
2024-06-25 11:54:52 +0630

    testing tailwindcss modal dialog

--- src/app/pages/managements/category-management/category-management.component.ts
+++ src/app/pages/managements/category-management/category-management.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-category-management',
   standalone: true,
   imports: [],
   templateUrl: './category-management.component.html',
-  styles: ``
+  styles: ``,
+  encapsulation: ViewEncapsulation.None
 })
 export class CategoryManagementComponent {
 
